a3b8b37a121ae618d75d8deefad8828df260b9bd
mvdwaeter <mvdwaeter@gmail.com>
2012-02-21 11:38:47 +0100

    Update settings_example.py

--- settings_example.py
+++ settings_example.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql',   # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'fiber_example',                # Or path to database file if using sqlite3.
         'USER': '',                             # Not used with sqlite3.
         'PASSWORD': '',                         # Not used with sqlite3.
